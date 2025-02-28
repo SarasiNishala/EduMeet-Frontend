b44a894f44386e2b36aac1d422d5778960e6f29f
Sarasi Nishala <sarasinishala@gmail.com>
2025-02-28 15:58:20 +0530

    Home page completed

--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -3,14 +3,18 @@ import Hero from '../../components/student/Hero'
 import Companies from '../../components/student/Companies'
 import CourseSection from '../../components/student/CourseSection'
 import TestimonialsSection from '../../components/student/TestimonialsSection'
+import CallTOAction from '../../components/student/CallTOAction'
+import Footer from '../../components/student/Footer'
 
 const Home = () => {
   return (
-    <div className='flex flex-col items-center space-y-7'>
+    <div className='flex flex-col items-center text-center space-y-7'>
         <Hero />
         <Companies />
         <CourseSection />
         <TestimonialsSection />
+        <CallTOAction />
+        <Footer />
     </div>
   )
 }
