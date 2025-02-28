cf26c40956b2e76426786d48abdda7f83588a8b0
Sarasi Nishala <sarasinishala@gmail.com>
2025-02-28 15:17:17 +0530

    Testimonial Section done

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     const {openSignIn} = useClerk()
     const {user} = useUser()
 
-    const {navigate} = useContext(AppContext)
+    const {navigate , isEducator} = useContext(AppContext)
 
   return (
     <>
@@ -20,7 +20,7 @@ const Navbar = () => {
         <div className='hidden md:flex items-center gap-5 text-gray-500'> 
             <div className='flex items-center gap-5'>
                 { user && <>
-                    <button>Become Educator</button>
+                    <button onClick={() => {navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
                     <Link to='/my-enrollments' >My Enrollments</Link>
                 </>}
             </div>
@@ -32,7 +32,7 @@ const Navbar = () => {
     <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
         <div className='flex items-center gap-1 sm:gap-2 max-sm:text-xs'>
             { user && <>
-                <button>Become Educator</button>
+                <button onClick={() => {navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
                 <Link to='/my-enrollments' >My Enrollments</Link>
             </>}
         </div>
