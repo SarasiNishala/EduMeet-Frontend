168404f8fe57a180b1c7ed03f30a62b65c7ade81
Sarasi Nishala <sarasinishala@gmail.com>
2025-02-28 09:15:56 +0530

    Search bar created

--- client/src/components/student/Hero.jsx
+++ client/src/components/student/Hero.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
 import { assets } from '../../assets/assets'
+import SearchBar from './SearchBar'
+
 
 const Hero = () => {
+
   return (
     <div className='flex flex-col items-center justify-center w-full md:pt-36 pt-36 pt-20 px-7 md:px-0 space-y-7 text-center bg-gradient-to-b from-cyan-100//70'>
         <h1 className='text-3xl md:text-5xl font-bold text-home-heading-small relative font-bold text-gray-800 max-w-3xl mx-auto'>Empover your future with the courses designed to <span className='text-blue-600'> fit your choice.</span>
@@ -19,6 +21,7 @@ const Hero = () => {
         <p className='md:hidden text-gray-500 max-w-sm mx-auto'>
             Access expert-led courses, learn freely, and boost your skills. Start today!
         </p>
+        <SearchBar />
     </div>
   )
 }
